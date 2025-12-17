c10312cf8e33c6002e2d74c5f3aad29aaddff1ff
bohdanssl <bohdan.sosnylo.shi.2024@lpnu.ua>
2025-12-17 10:46:32 +0200

    Delete lab4 directory

--- lab4/main/repositories/repomanager.py
+++
@@ -1,9 +0,0 @@
-from .baserepo import BaseRepository
-from .ticketrepo import TicketRepository
-from main.models import Passenger, Train
-
-class RepositoryManager:
-    def __init__(self):
-        self.passengers = BaseRepository(Passenger)
-        self.trains = BaseRepository(Train)
-        self.tickets = TicketRepository()
